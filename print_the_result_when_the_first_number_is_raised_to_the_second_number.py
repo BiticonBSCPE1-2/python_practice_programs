6ddcf50616fdffbed0c8625dd156e600004f055e
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-12 23:45:11 +0800

    Prints the value of the first number raised to the second number

--- print_the_result_when_the_first_number_is_raised_to_the_second_number.py
+++ print_the_result_when_the_first_number_is_raised_to_the_second_number.py
@@ -1,2 +1,6 @@
 # Input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
 # Print the value of the first number raised to the power of the second number
+print(num1 ** num2)
