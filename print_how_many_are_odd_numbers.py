d89c093ff9b0287156b658fa13d249c668ab1c6f
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-12 23:57:46 +0800

    Prints how many are odd numbers in those ten inputs

--- print_how_many_are_odd_numbers.py
+++ print_how_many_are_odd_numbers.py
@@ -1,2 +1,11 @@
 # Input 10 numbers
+count = 0
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
+    if num % 2 != 0:
+        count += 1
+    else:
+        print("It is an even number")
+
 # Prints how many of the numbers are odd
+print(count)
