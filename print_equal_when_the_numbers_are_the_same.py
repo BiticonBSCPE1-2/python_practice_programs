89f2e9482aae9e9471e9e7d383468e3afa514192
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-12 23:33:24 +0800

    Prints Equal if the numbers are equal and not if its not equal

--- print_equal_when_the_numbers_are_the_same.py
+++ print_equal_when_the_numbers_are_the_same.py
@@ -1,2 +1,9 @@
 # Input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
 # Output "Numbers are Equal" if equal and "Numbers are not Equal" if not equal
+if num1 == num2:
+    print("Numbers are equal")
+else:
+    print("They are not equal")
