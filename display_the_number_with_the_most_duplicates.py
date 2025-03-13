e13af18f7b1831dc79dcd89fdf3ef8f1b385377d
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 23:31:11 +0800

    Outputs the most duplicate number

--- display_the_number_with_the_most_duplicates.py
+++ display_the_number_with_the_most_duplicates.py
@@ -1,3 +1,21 @@
 # input numbers
+numbers = []
+duplicate_numbers = {}
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+
 # Checks for duplicates
+        if num in duplicate_numbers:
+            duplicate_numbers[num] += 1
+        else:
+            duplicate_numbers[num] = 1
+    except ValueError:
+        break
+
 # Outputs the number with the most duplicates
+if numbers:
+    most_duplicates = max(duplicate_numbers, key=duplicate_numbers.get)
+    print(most_duplicates)
