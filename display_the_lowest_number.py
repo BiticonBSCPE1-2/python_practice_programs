6f65bc53a51ae7671212ce714921a384b8ca9f66
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 23:06:38 +0800

    Displays lowest number until invalid input

--- display_the_lowest_number.py
+++ display_the_lowest_number.py
@@ -1,3 +1,19 @@
 # Input a number
+lowest_number = None
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+
 # Checks the lowest number
+        if lowest_number is None or num < lowest_number:
+            lowest_number = num
+    except ValueError:
+        break
+
 # Display the lowest number
+
+if lowest_number is not None:
+    print(lowest_number)
+else:
+    print("Invalid input")
