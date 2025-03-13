d74f0719204c716c4a429711feafe51710f27162
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 23:19:03 +0800

    Displays Duplicate numbers

--- display_all_numbers_that_have_duplicates.py
+++ display_all_numbers_that_have_duplicates.py
@@ -1,3 +1,15 @@
 # input 10 numbers
+numbers = []
+duplicate_numbers = set()
+
+for i in range(10):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
+
 # checks numbers for duplicates
+for num in numbers:    
+    if numbers.count(num) > 1:
+        duplicate_numbers.add(num)
+
 # displays numbers that have duplicates
+print(duplicate_numbers)
