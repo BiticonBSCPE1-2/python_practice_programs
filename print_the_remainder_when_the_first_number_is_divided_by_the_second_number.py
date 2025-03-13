3f764a11f5f8646c239bddbb6dc3b60953d6ec4d
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 20:10:10 +0800

    Outputs the remainder of the two numbers

--- print_the_remainder_when_the_first_number_is_divided_by_the_second_number.py
+++ print_the_remainder_when_the_first_number_is_divided_by_the_second_number.py
@@ -1,2 +1,6 @@
 # Input two numbers
-# Print the product of the two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Print the remainder when the first number is divided by the second number
+print(num1 % num2)
