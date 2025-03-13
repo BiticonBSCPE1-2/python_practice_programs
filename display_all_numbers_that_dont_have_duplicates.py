87e587a4c53dd7c3250d40d0d7b7675b9f31c782
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 22:30:57 +0800

    Working input

--- display_all_numbers_that_dont_have_duplicates.py
+++ display_all_numbers_that_dont_have_duplicates.py
@@ -1,3 +1,10 @@
 # input 10 numbers
+numbers = []
+duplicate_numbers = []
+
+for i in range(10):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
+
 # checks numbers for duplicates
 # displays numbers that don't have duplicates
