295d0ae3757828a377a96d03a8d1fa26d854ad8e
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 23:36:04 +0800

    Displays the highest number

--- display_the_highest_number.py
+++ display_the_highest_number.py
@@ -1,3 +1,18 @@
 # input numbers
+highest_number = None
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        
 # Checks for the highest number
+        if highest_number is None or num > highest_number:
+            highest_number = num
+    except ValueError:
+        break
+
 # Displays the highest number
+if highest_number is not None:
+    print(highest_number)
+else:
+    print("Invalid input")
