ddbbea2bddc09b90ce5ca85e15d8ec09719dff67
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 23:43:25 +0800

    Outputs the average with the input until an invalid input occured

--- display_the_average.py
+++ display_the_average.py
@@ -1,2 +1,16 @@
 # input numbers
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
+
 # calculates the average of the numbers
+if numbers:
+    average = sum(numbers) / len(numbers)
+    print(average)
+else:
+    print("Invalid input")
