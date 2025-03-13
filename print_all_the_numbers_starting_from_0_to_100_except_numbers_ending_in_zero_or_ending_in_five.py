db37192af0a9f98a9be42c6b411b0f1eca6b5e34
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 21:29:35 +0800

    Outputs numbers from 0 to 100 except ending in zero or 5

--- print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero_or_ending_in_five.py
+++ print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero_or_ending_in_five.py
@@ -1,1 +1,6 @@
 # print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero_or_ending_in_five
+num = 0
+while num <= 100:
+    if num % 10 != 0 and num % 10 != 5:
+        print(num)
+    num += 1
