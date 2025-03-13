8484493275018152b0a4064303fc05d642762c55
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 19:49:31 +0800

    Outputs not equal if the input is not equal, otherwise, it is equal

--- print_not_equal_when_the_numbers_are_not_the_same.py
+++ print_not_equal_when_the_numbers_are_not_the_same.py
@@ -1,2 +1,9 @@
 # Input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
 # Output "Numbers are not Equal" if not equal
+if num1 != num2:
+    print("Numbers are not Equal")
+else:
+    print("Numbers are Equal")
