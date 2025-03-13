db7cce0969cceaefaebd4c107e68031624a4b2f1
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 23:00:48 +0800

    Outputs Unique and duplicate properly and loop ends if its an invalid input

--- display_unique_after_input_when_the_inputted_number_does_not_have_a_duplicate.py
+++ display_unique_after_input_when_the_inputted_number_does_not_have_a_duplicate.py
@@ -1,2 +1,15 @@
 # Input a number
-# Check if number is unique or duplicate
+unique_numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+
+# Check if number is unique or duplicate and loops until user enters a non-integer
+        if num in unique_numbers:
+            print("Duplicate")
+        else:
+            print("Unique")
+            unique_numbers.append(num)
+    except ValueError:
+        break
