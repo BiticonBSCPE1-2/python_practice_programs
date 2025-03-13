0907b4781895c232a33ad74c04ff2660ebe43121
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 23:10:29 +0800

    Sorts the numbers to ascending order

--- display_the_numbers_from_lowest_to_highest.py
+++ display_the_numbers_from_lowest_to_highest.py
@@ -1,2 +1,12 @@
 # Input numbers
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
 # Sorts numbers from lowest to highest
+numbers.sort()
+print(numbers)
