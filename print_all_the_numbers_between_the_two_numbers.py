22cce4051d2be39b52934cee6c185178352668b4
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 21:42:09 +0800

    Outputs the numbers between the two numbers

--- print_all_the_numbers_between_the_two_numbers.py
+++ print_all_the_numbers_between_the_two_numbers.py
@@ -1,2 +1,12 @@
 # input two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
 # Prints the numbers between two numbers
+if num1 > num2:
+    num1, num2 = num1, num2
+
+num1 += 1
+while num1 < num2:
+    print(num1)
+    num1 += 1
