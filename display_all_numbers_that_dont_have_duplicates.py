374e7b6a317cd83cd9c2cd843b0056419ea6fd31
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 22:36:12 +0800

    Displays none duplicate numebrs

--- display_all_numbers_that_dont_have_duplicates.py
+++ display_all_numbers_that_dont_have_duplicates.py
@@ -7,4 +7,9 @@ for i in range(10):
     numbers.append(num)
 
 # checks numbers for duplicates
+for num in numbers:    
+    if numbers.count(num) == 1:
+        duplicate_numbers.append(num)
+
 # displays numbers that don't have duplicates
+print(duplicate_numbers)
