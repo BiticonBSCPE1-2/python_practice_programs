cc465f92add514c0860f4d1d6dc5e84ce5171c05
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-13 20:31:42 +0800

    Outputs the value of the first number minus all of the remaining numbers

--- print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
+++ print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
@@ -1,2 +1,11 @@
 # Input 10 numbers
+total = 0
+for i in range(10):
+    num = float(input(f"Enter number {i+1}: "))
+    if i == 0:
+        total += num
+    else:
+        total -= num
+
 # Print the value of the first number minus all of the remaining numbers
+print(total)
